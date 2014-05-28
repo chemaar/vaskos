0c0034bd31a60b9a62b83b3ee646d267e658d9b1
chemaar <chema.ar@gmail.com>
2014-05-28 21:42:14 +0200

    Moving files

--- trunk/vaskos/src/test/java/es/inf/uc3m/kr/validator/impl/SKOSValidatorSPARQLImplTest.java
+++ trunk/vaskos/src/test/java/es/inf/uc3m/kr/validator/impl/SKOSValidatorSPARQLImplTest.java
@@ -13,7 +13,7 @@ public class SKOSValidatorSPARQLImplTest {
 
 	public void testAccept() throws IOException {
 		String rdfFile = "skos/sparql/test/accepted/mountain-bike-simple.ttl";
-		String[] sparqlFiles = new String []{"src/test/resources/skos/sparql/test/rules/s7.sparql"};
+		String[] sparqlFiles = new String []{"src/main/resources/skos/sparql/rules/s7.sparql"};
 		SKOSValidatorSPARQLImpl validator = new SKOSValidatorSPARQLImpl(sparqlFiles);
 		Assert.assertTrue(validator.validate(rdfFile));
 	}
@@ -21,7 +21,7 @@ public class SKOSValidatorSPARQLImplTest {
 	@Test
 	public void testAccept_Consistent_8() throws IOException {
 		String rdfFile = "skos/test/sources/accepted/consistent-8.ttl";
-		String[] sparqlFiles = new String []{"src/test/resources/skos/sparql/test/rules/consistent-8.sparql"};
+		String[] sparqlFiles = new String []{"src/main/resources/skos/sparql/rules/consistent-8.sparql"};
 		SKOSValidatorSPARQLImpl validator = new SKOSValidatorSPARQLImpl(sparqlFiles);
 		Assert.assertTrue(validator.validate(rdfFile));
 	}
@@ -29,7 +29,7 @@ public class SKOSValidatorSPARQLImplTest {
 	@Test
 	public void testAccept_Consistent_9() throws IOException {
 		String rdfFile = "skos/test/sources/accepted/consistent-9.ttl";
-		String[] sparqlFiles = new String []{"src/test/resources/skos/sparql/test/rules/consistent-9.sparql"};
+		String[] sparqlFiles = new String []{"src/main/resources/skos/sparql/rules/consistent-9.sparql"};
 		SKOSValidatorSPARQLImpl validator = new SKOSValidatorSPARQLImpl(sparqlFiles);
 		Assert.assertTrue(validator.validate(rdfFile));
 	}
@@ -37,7 +37,7 @@ public class SKOSValidatorSPARQLImplTest {
 	@Test
 	public void testReject_12() throws IOException {
 		String rdfFile = "skos/test/sources/rejected/rejected-12.ttl";
-		String[] sparqlFiles = new String []{"src/test/resources/skos/sparql/test/rules/s13.sparql"};
+		String[] sparqlFiles = new String []{"src/main/resources/skos/sparql/rules/s13.sparql"};
 		SKOSValidatorSPARQLImpl validator = new SKOSValidatorSPARQLImpl(sparqlFiles);
 		Assert.assertFalse(validator.validate(rdfFile));
 	}
@@ -45,7 +45,7 @@ public class SKOSValidatorSPARQLImplTest {
 	@Test
 	public void testReject_13() throws IOException {
 		String rdfFile = "skos/test/sources/rejected/rejected-13.ttl";
-		String[] sparqlFiles = new String []{"src/test/resources/skos/sparql/test/rules/s13.sparql"};
+		String[] sparqlFiles = new String []{"src/main/resources/skos/sparql/rules/s13.sparql"};
 		SKOSValidatorSPARQLImpl validator = new SKOSValidatorSPARQLImpl(sparqlFiles);
 		Assert.assertFalse(validator.validate(rdfFile));
 	}
@@ -53,7 +53,7 @@ public class SKOSValidatorSPARQLImplTest {
 	@Test
 	public void testReject_14() throws IOException {
 		String rdfFile = "skos/test/sources/rejected/rejected-14.ttl";
-		String[] sparqlFiles = new String []{"src/test/resources/skos/sparql/test/rules/s13.sparql"};
+		String[] sparqlFiles = new String []{"src/main/resources/skos/sparql/rules/s13.sparql"};
 		SKOSValidatorSPARQLImpl validator = new SKOSValidatorSPARQLImpl(sparqlFiles);
 		Assert.assertFalse(validator.validate(rdfFile));
 	}
@@ -61,7 +61,7 @@ public class SKOSValidatorSPARQLImplTest {
 	@Test
 	public void testReject_15() throws IOException {
 		String rdfFile = "skos/test/sources/rejected/rejected-15.ttl";
-		String[] sparqlFiles = new String []{"src/test/resources/skos/sparql/test/rules/s13.sparql"};
+		String[] sparqlFiles = new String []{"src/main/resources/skos/sparql/rules/s13.sparql"};
 		SKOSValidatorSPARQLImpl validator = new SKOSValidatorSPARQLImpl(sparqlFiles);
 		Assert.assertFalse(validator.validate(rdfFile));
 	}
@@ -70,7 +70,7 @@ public class SKOSValidatorSPARQLImplTest {
 	@Test
 	public void testAccept_S14() throws IOException {
 		String rdfFile = "skos/test/sources/accepted/s14.ttl";
-		String[] sparqlFiles = new String []{"src/test/resources/skos/sparql/test/rules/s14.sparql"};
+		String[] sparqlFiles = new String []{"src/main/resources/skos/sparql/rules/s14.sparql"};
 		SKOSValidatorSPARQLImpl validator = new SKOSValidatorSPARQLImpl(sparqlFiles);
 		Assert.assertTrue(validator.validate(rdfFile));
 	}
@@ -78,7 +78,7 @@ public class SKOSValidatorSPARQLImplTest {
 	@Test
 	public void testReject_S14() throws IOException {
 		String rdfFile = "skos/test/sources/rejected/rejected-s14.ttl";
-		String[] sparqlFiles = new String []{"src/test/resources/skos/sparql/test/rules/s14.sparql"};
+		String[] sparqlFiles = new String []{"src/main/resources/skos/sparql/rules/s14.sparql"};
 		SKOSValidatorSPARQLImpl validator = new SKOSValidatorSPARQLImpl(sparqlFiles);
 		Assert.assertFalse(validator.validate(rdfFile));
 	}
@@ -88,7 +88,7 @@ public class SKOSValidatorSPARQLImplTest {
 	@Test
 	public void testAccept_Consistent_10() throws IOException {
 		String rdfFile = "skos/test/sources/accepted/consistent-10.ttl";
-		String[] sparqlFiles = new String []{"src/test/resources/skos/sparql/test/rules/s14.sparql"};
+		String[] sparqlFiles = new String []{"src/main/resources/skos/sparql/rules/s14.sparql"};
 		SKOSValidatorSPARQLImpl validator = new SKOSValidatorSPARQLImpl(sparqlFiles);
 		Assert.assertTrue(validator.validate(rdfFile));
 	}
@@ -96,7 +96,7 @@ public class SKOSValidatorSPARQLImplTest {
 	@Test
 	public void testAccept_Consistent_11() throws IOException {
 		String rdfFile = "skos/test/sources/accepted/consistent-11.ttl";
-		String[] sparqlFiles = new String []{"src/test/resources/skos/sparql/test/rules/s14.sparql"};
+		String[] sparqlFiles = new String []{"src/main/resources/skos/sparql/rules/s14.sparql"};
 		SKOSValidatorSPARQLImpl validator = new SKOSValidatorSPARQLImpl(sparqlFiles);
 		Assert.assertTrue(validator.validate(rdfFile));
 	}
@@ -104,7 +104,7 @@ public class SKOSValidatorSPARQLImplTest {
 	@Test
 	public void testAccept_Consistent_16() throws IOException {
 		String rdfFile = "skos/test/sources/accepted/consistent-16.ttl";
-		String[] sparqlFiles = new String []{"src/test/resources/skos/sparql/test/rules/s13.sparql"};
+		String[] sparqlFiles = new String []{"src/main/resources/skos/sparql/rules/s13.sparql"};
 		SKOSValidatorSPARQLImpl validator = new SKOSValidatorSPARQLImpl(sparqlFiles);
 		Assert.assertTrue(validator.validate(rdfFile));
 	}
@@ -112,7 +112,7 @@ public class SKOSValidatorSPARQLImplTest {
 	@Test
 	public void testAccept_Consistent_17() throws IOException {
 		String rdfFile = "skos/test/sources/accepted/consistent-17.ttl";
-		String[] sparqlFiles = new String []{"src/test/resources/skos/sparql/test/rules/s13.sparql"};
+		String[] sparqlFiles = new String []{"src/main/resources/skos/sparql/rules/s13.sparql"};
 		SKOSValidatorSPARQLImpl validator = new SKOSValidatorSPARQLImpl(sparqlFiles);
 		Assert.assertTrue(validator.validate(rdfFile));
 	}
@@ -121,7 +121,7 @@ public class SKOSValidatorSPARQLImplTest {
 	@Test
 	public void testAccept_Consistent_18() throws IOException {
 		String rdfFile = "skos/test/sources/accepted/consistent-18.ttl";
-		String[] sparqlFiles = new String []{"src/test/resources/skos/sparql/test/rules/s13.sparql"};
+		String[] sparqlFiles = new String []{"src/main/resources/skos/sparql/rules/s13.sparql"};
 		SKOSValidatorSPARQLImpl validator = new SKOSValidatorSPARQLImpl(sparqlFiles);
 		Assert.assertTrue(validator.validate(rdfFile));
 	}
@@ -130,7 +130,7 @@ public class SKOSValidatorSPARQLImplTest {
 	@Test
 	public void testAccept_Consistent_19() throws IOException {
 		String rdfFile = "skos/test/sources/accepted/consistent-19.ttl";
-		String[] sparqlFiles = new String []{"src/test/resources/skos/sparql/test/rules/s13.sparql"};
+		String[] sparqlFiles = new String []{"src/main/resources/skos/sparql/rules/s13.sparql"};
 		SKOSValidatorSPARQLImpl validator = new SKOSValidatorSPARQLImpl(sparqlFiles);
 		Assert.assertTrue(validator.validate(rdfFile));
 	}
@@ -138,7 +138,7 @@ public class SKOSValidatorSPARQLImplTest {
 //	@Test
 //	public void testAccept_S25() throws IOException {
 //		String rdfFile = "skos/test/sources/accepted/s25.ttl";
-//		String[] sparqlFiles = new String []{"src/test/resources/skos/sparql/test/rules/s25.sparql"};
+//		String[] sparqlFiles = new String []{"src/main/resources/skos/sparql/rules/s25.sparql"};
 //		SKOSValidatorSPARQLImpl validator = new SKOSValidatorSPARQLImpl(sparqlFiles);
 //		Assert.assertTrue(validator.validate(rdfFile));
 //	}
@@ -151,7 +151,7 @@ public class SKOSValidatorSPARQLImplTest {
 	@Test
 	public void testReject_S26() throws IOException {
 		String rdfFile = "skos/test/sources/rejected/rejected-s26.ttl";
-		String[] sparqlFiles = new String []{"src/test/resources/skos/sparql/test/rules/s26.sparql"};
+		String[] sparqlFiles = new String []{"src/main/resources/skos/sparql/rules/s26.sparql"};
 		SKOSValidatorSPARQLImpl validator = new SKOSValidatorSPARQLImpl(sparqlFiles);
 		Assert.assertFalse(validator.validate(rdfFile));
 	}
@@ -159,7 +159,7 @@ public class SKOSValidatorSPARQLImplTest {
 	@Test
 	public void testReject_S26_1() throws IOException {
 		String rdfFile = "skos/test/sources/rejected/rejected-s26-1.ttl";
-		String[] sparqlFiles = new String []{"src/test/resources/skos/sparql/test/rules/s26-1.sparql"};
+		String[] sparqlFiles = new String []{"src/main/resources/skos/sparql/rules/s26-1.sparql"};
 		SKOSValidatorSPARQLImpl validator = new SKOSValidatorSPARQLImpl(sparqlFiles);
 		Assert.assertFalse(validator.validate(rdfFile));
 	}
@@ -168,7 +168,7 @@ public class SKOSValidatorSPARQLImplTest {
 	@Test
 	public void testReject_S26_2() throws IOException {
 		String rdfFile = "skos/test/sources/rejected/rejected-s26-2.ttl";
-		String[] sparqlFiles = new String []{"src/test/resources/skos/sparql/test/rules/s26-2.sparql"};
+		String[] sparqlFiles = new String []{"src/main/resources/skos/sparql/rules/s26-2.sparql"};
 		SKOSValidatorSPARQLImpl validator = new SKOSValidatorSPARQLImpl(sparqlFiles);
 		Assert.assertFalse(validator.validate(rdfFile));
 	}
@@ -178,7 +178,7 @@ public class SKOSValidatorSPARQLImplTest {
 	@Test
 	public void testReject_S26_3() throws IOException {
 		String rdfFile = "skos/test/sources/rejected/rejected-s26-3.ttl";
-		String[] sparqlFiles = new String []{"src/test/resources/skos/sparql/test/rules/s26-3.sparql"};
+		String[] sparqlFiles = new String []{"src/main/resources/skos/sparql/rules/s26-3.sparql"};
 		SKOSValidatorSPARQLImpl validator = new SKOSValidatorSPARQLImpl(sparqlFiles);
 		Assert.assertFalse(validator.validate(rdfFile));
 	}
@@ -188,7 +188,7 @@ public class SKOSValidatorSPARQLImplTest {
 	@Test
 	public void testReject_S27() throws IOException {
 		String rdfFile = "skos/test/sources/rejected/rejected-s27.ttl";
-		String[] sparqlFiles = new String []{"src/test/resources/skos/sparql/test/rules/s27.sparql"};
+		String[] sparqlFiles = new String []{"src/main/resources/skos/sparql/rules/s27.sparql"};
 		SKOSValidatorSPARQLImpl validator = new SKOSValidatorSPARQLImpl(sparqlFiles);
 		Assert.assertFalse(validator.validate(rdfFile));
 	}
@@ -197,7 +197,7 @@ public class SKOSValidatorSPARQLImplTest {
 	@Test
 	public void testReject_S27_1() throws IOException {
 		String rdfFile = "skos/test/sources/rejected/rejected-s27-1.ttl";
-		String[] sparqlFiles = new String []{"src/test/resources/skos/sparql/test/rules/s27-1.sparql"};
+		String[] sparqlFiles = new String []{"src/main/resources/skos/sparql/rules/s27-1.sparql"};
 		SKOSValidatorSPARQLImpl validator = new SKOSValidatorSPARQLImpl(sparqlFiles);
 		Assert.assertFalse(validator.validate(rdfFile));
 	}
@@ -205,7 +205,7 @@ public class SKOSValidatorSPARQLImplTest {
 	@Test
 	public void testReject_S27_2() throws IOException {
 		String rdfFile = "skos/test/sources/rejected/rejected-s27-2.ttl";
-		String[] sparqlFiles = new String []{"src/test/resources/skos/sparql/test/rules/s27-2.sparql"};
+		String[] sparqlFiles = new String []{"src/main/resources/skos/sparql/rules/s27-2.sparql"};
 		SKOSValidatorSPARQLImpl validator = new SKOSValidatorSPARQLImpl(sparqlFiles);
 		Assert.assertFalse(validator.validate(rdfFile));
 	}
@@ -213,7 +213,7 @@ public class SKOSValidatorSPARQLImplTest {
 	@Test
 	public void testReject_S27_3() throws IOException {
 		String rdfFile = "skos/test/sources/rejected/rejected-s27-3.ttl";
-		String[] sparqlFiles = new String []{"src/test/resources/skos/sparql/test/rules/s27-3.sparql"};
+		String[] sparqlFiles = new String []{"src/main/resources/skos/sparql/rules/s27-3.sparql"};
 		SKOSValidatorSPARQLImpl validator = new SKOSValidatorSPARQLImpl(sparqlFiles);
 		Assert.assertFalse(validator.validate(rdfFile));
 	}
@@ -221,7 +221,7 @@ public class SKOSValidatorSPARQLImplTest {
 	@Test
 	public void testReject_S27_4() throws IOException {
 		String rdfFile = "skos/test/sources/rejected/rejected-s27-4";
-		String[] sparqlFiles = new String []{"src/test/resources/skos/sparql/test/rules/s27-4.sparql"};
+		String[] sparqlFiles = new String []{"src/main/resources/skos/sparql/rules/s27-4.sparql"};
 		SKOSValidatorSPARQLImpl validator = new SKOSValidatorSPARQLImpl(sparqlFiles);
 		for(int i = 1; i<=4; i++){
 			Assert.assertFalse(validator.validate(rdfFile+"-"+i+".ttl"));
@@ -232,7 +232,7 @@ public class SKOSValidatorSPARQLImplTest {
 	@Test
 	public void testReject_S27_5() throws IOException {
 		String rdfFile = "skos/test/sources/rejected/rejected-s27-5";
-		String[] sparqlFiles = new String []{"src/test/resources/skos/sparql/test/rules/s27-5.sparql"};
+		String[] sparqlFiles = new String []{"src/main/resources/skos/sparql/rules/s27-5.sparql"};
 		SKOSValidatorSPARQLImpl validator = new SKOSValidatorSPARQLImpl(sparqlFiles);
 		for(int i = 1; i<=4; i++){
 			Assert.assertFalse(validator.validate(rdfFile+"-"+i+".ttl"));
@@ -243,7 +243,7 @@ public class SKOSValidatorSPARQLImplTest {
 	@Test
 	public void testReject_S27_6() throws IOException {
 		String rdfFile = "skos/test/sources/rejected/rejected-s27-6.ttl";
-		String[] sparqlFiles = new String []{"src/test/resources/skos/sparql/test/rules/s27-6.sparql"};
+		String[] sparqlFiles = new String []{"src/main/resources/skos/sparql/rules/s27-6.sparql"};
 		SKOSValidatorSPARQLImpl validator = new SKOSValidatorSPARQLImpl(sparqlFiles);
 		Assert.assertFalse(validator.validate(rdfFile));
 	}
--- trunk/vaskos/src/test/java/es/inf/uc3m/kr/validator/impl/SKOSValidatorShexImplTest.java
+++ trunk/vaskos/src/test/java/es/inf/uc3m/kr/validator/impl/SKOSValidatorShexImplTest.java
@@ -68,7 +68,7 @@ public class SKOSValidatorShexImplTest {
 	public void testAccept_Consistent_2() {
 		try{
 			URI iri = new URI("http://example.org/MyConcept");
-			String shexFile = "src/test/resources/skos/test/shex/rules/skos-concept.shex";
+			String shexFile = "src/main/resources/skos/shex/rules/skos-concept.shex";
 			String rdfFile = "src/test/resources/skos/test/sources/accepted/consistent-2.ttl";
 			Assert.assertTrue(executeTest(iri, shexFile, rdfFile));
 		}catch (Exception e){
@@ -82,7 +82,7 @@ public class SKOSValidatorShexImplTest {
 	public void testAccept_Consistent_3() {
 		try{
 			URI iri = new URI("http://example.org/MyConcept");
-			String shexFile = "src/test/resources/skos/test/shex/rules/skos-concept.shex";
+			String shexFile = "src/main/resources/skos/shex/rules/skos-concept.shex";
 			String rdfFile = "src/test/resources/skos/test/sources/accepted/consistent-3.ttl";
 			Assert.assertTrue(executeTest(iri, shexFile, rdfFile));
 		}catch (Exception e){
@@ -96,7 +96,7 @@ public class SKOSValidatorShexImplTest {
 	public void testAccept_Consistent_4() {
 		try{
 			URI iri = new URI("http://example.org/MyConcept");
-			String shexFile = "src/test/resources/skos/test/shex/rules/skos-concept.shex";
+			String shexFile = "src/main/resources/skos/shex/rules/skos-concept.shex";
 			String rdfFile = "src/test/resources/skos/test/sources/accepted/consistent-4.ttl";
 			Assert.assertTrue(executeTest(iri, shexFile, rdfFile));
 		}catch (Exception e){
@@ -110,7 +110,7 @@ public class SKOSValidatorShexImplTest {
 	public void testAccept_Consistent_S2() {
 		try{
 			URI iri = new URI("http://example.org/MyConcept");
-			String shexFile = "src/test/resources/skos/test/shex/rules/skos-concept.shex";
+			String shexFile = "src/main/resources/skos/shex/rules/skos-concept.shex";
 			String rdfFile = "src/test/resources/skos/test/sources/accepted/s2.ttl";
 			Assert.assertTrue(executeTest(iri, shexFile, rdfFile));
 		}catch (Exception e){
@@ -123,7 +123,7 @@ public class SKOSValidatorShexImplTest {
 	public void testAccept_Consistent_S3() {
 		try{
 			URI iri = new URI("http://example.org/MyConcept");
-			String shexFile = "src/test/resources/skos/test/shex/rules/skos-concept.shex";
+			String shexFile = "src/main/resources/skos/shex/rules/skos-concept.shex";
 			String rdfFile = "src/test/resources/skos/test/sources/accepted/s3.ttl";
 			Assert.assertTrue(executeTest(iri, shexFile, rdfFile));
 		}catch (Exception e){
@@ -137,7 +137,7 @@ public class SKOSValidatorShexImplTest {
 	public void testAccept_Consistent_S4() {
 		try{
 			URI iri = new URI("http://example.org/MyConcept");
-			String shexFile = "src/test/resources/skos/test/shex/rules/skos-concept.shex";
+			String shexFile = "src/main/resources/skos/shex/rules/skos-concept.shex";
 			String rdfFile = "src/test/resources/skos/test/sources/accepted/s4.ttl";
 			Assert.assertTrue(executeTest(iri, shexFile, rdfFile));
 		}catch (Exception e){
@@ -150,7 +150,7 @@ public class SKOSValidatorShexImplTest {
 	public void testAccept_Consistent_S5() {
 		try{
 			URI iri = new URI("http://example.org/MyConcept");
-			String shexFile = "src/test/resources/skos/test/shex/rules/skos-concept.shex";
+			String shexFile = "src/main/resources/skos/shex/rules/skos-concept.shex";
 			String rdfFile = "src/test/resources/skos/test/sources/accepted/s5.ttl";
 			Assert.assertTrue(executeTest(iri, shexFile, rdfFile));
 		}catch (Exception e){
@@ -164,7 +164,7 @@ public class SKOSValidatorShexImplTest {
 	public void testAccept_Consistent_S6() {
 		try{
 			URI iri = new URI("http://example.org/MyConcept");
-			String shexFile = "src/test/resources/skos/test/shex/rules/skos-concept.shex";
+			String shexFile = "src/main/resources/skos/shex/rules/skos-concept.shex";
 			String rdfFile = "src/test/resources/skos/test/sources/accepted/s6.ttl";
 			Assert.assertTrue(executeTest(iri, shexFile, rdfFile));
 		}catch (Exception e){
@@ -177,7 +177,7 @@ public class SKOSValidatorShexImplTest {
 	public void testAccept_Consistent_5() {
 		try{
 			URI iri = new URI("http://example.org/MyConcept");
-			String shexFile = "src/test/resources/skos/test/shex/rules/skos-concept.shex";
+			String shexFile = "src/main/resources/skos/shex/rules/skos-concept.shex";
 			String rdfFile = "src/test/resources/skos/test/sources/accepted/consistent-5.ttl";
 			Assert.assertTrue(executeTest(iri, shexFile, rdfFile));
 		}catch (Exception e){
@@ -191,7 +191,7 @@ public class SKOSValidatorShexImplTest {
 	public void testAccept_Consistent_6() {
 		try{
 			URI iri = new URI("http://example.org/MyConcept");
-			String shexFile = "src/test/resources/skos/test/shex/rules/skos-concept.shex";
+			String shexFile = "src/main/resources/skos/shex/rules/skos-concept.shex";
 			String rdfFile = "src/test/resources/skos/test/sources/accepted/consistent-6.ttl";
 			Assert.assertTrue(executeTest(iri, shexFile, rdfFile));
 		}catch (Exception e){
@@ -205,7 +205,7 @@ public class SKOSValidatorShexImplTest {
 	public void testAccept_Consistent_7() {
 		try{
 			URI iri = new URI("http://example.org/MyConcept");
-			String shexFile = "src/test/resources/skos/test/shex/rules/skos-concept.shex";
+			String shexFile = "src/main/resources/skos/shex/rules/skos-concept.shex";
 			String rdfFile = "src/test/resources/skos/test/sources/accepted/consistent-7.ttl";
 			Assert.assertTrue(executeTest(iri, shexFile, rdfFile));
 		}catch (Exception e){
@@ -220,7 +220,7 @@ public class SKOSValidatorShexImplTest {
 	public void testAccept_Consistent_8() {
 		try{
 			URI iri = new URI("http://example.org/MyConcept");
-			String shexFile = "src/test/resources/skos/test/shex/rules/skos-concept.shex";
+			String shexFile = "src/main/resources/skos/shex/rules/skos-concept.shex";
 			String rdfFile = "src/test/resources/skos/test/sources/accepted/consistent-8.ttl";
 			Assert.assertTrue(executeTest(iri, shexFile, rdfFile));
 		}catch (Exception e){
@@ -234,7 +234,7 @@ public class SKOSValidatorShexImplTest {
 	public void testAccept_Consistent_9() {
 		try{
 			URI iri = new URI("http://example.org/MyConcept");
-			String shexFile = "src/test/resources/skos/test/shex/rules/skos-concept.shex";
+			String shexFile = "src/main/resources/skos/shex/rules/skos-concept.shex";
 			String rdfFile = "src/test/resources/skos/test/sources/accepted/consistent-9.ttl";
 			Assert.assertTrue(executeTest(iri, shexFile, rdfFile));
 		}catch (Exception e){
@@ -248,7 +248,7 @@ public class SKOSValidatorShexImplTest {
 	public void testAccept_Consistent_10() {
 		try{
 			URI iri = new URI("http://example.org/MyConcept");
-			String shexFile = "src/test/resources/skos/test/shex/rules/skos-concept.shex";
+			String shexFile = "src/main/resources/skos/shex/rules/skos-concept.shex";
 			String rdfFile = "src/test/resources/skos/test/sources/accepted/consistent-10.ttl";
 			Assert.assertTrue(executeTest(iri, shexFile, rdfFile));
 		}catch (Exception e){
@@ -263,7 +263,7 @@ public class SKOSValidatorShexImplTest {
 	public void testAccept_S13() {
 		try{
 			URI iri = new URI("http://example.org/MyConcept");
-			String shexFile = "src/test/resources/skos/test/shex/rules/skos-concept.shex";
+			String shexFile = "src/main/resources/skos/shex/rules/skos-concept.shex";
 			String rdfFile = "src/test/resources/skos/test/sources/accepted/s13.ttl";
 			Assert.assertTrue(executeTest(iri, shexFile, rdfFile));
 		}catch (Exception e){
@@ -276,7 +276,7 @@ public class SKOSValidatorShexImplTest {
 	public void testAccept_16() {
 		try{
 			URI iri = new URI("http://example.org/MyClass");
-			String shexFile = "src/test/resources/skos/test/shex/rules/skos-concept.shex";
+			String shexFile = "src/main/resources/skos/shex/rules/skos-concept.shex";
 			String rdfFile = "src/test/resources/skos/test/sources/accepted/consistent-16.ttl";
 			Assert.assertTrue(executeTest(iri, shexFile, rdfFile));
 		}catch (Exception e){
@@ -290,7 +290,7 @@ public class SKOSValidatorShexImplTest {
 	public void testAccept_17() {
 		try{
 			URI iri = new URI("http://example.org/Love");
-			String shexFile = "src/test/resources/skos/test/shex/rules/skos-concept.shex";
+			String shexFile = "src/main/resources/skos/shex/rules/skos-concept.shex";
 			String rdfFile = "src/test/resources/skos/test/sources/accepted/consistent-17.ttl";
 			Assert.assertTrue(executeTest(iri, shexFile, rdfFile));
 		}catch (Exception e){
@@ -304,7 +304,7 @@ public class SKOSValidatorShexImplTest {
 	public void testAccept_18() {
 		try{
 			URI iri = new URI("http://example.org/Colour");
-			String shexFile = "src/test/resources/skos/test/shex/rules/skos-concept.shex";
+			String shexFile = "src/main/resources/skos/shex/rules/skos-concept.shex";
 			String rdfFile = "src/test/resources/skos/test/sources/accepted/consistent-18.ttl";
 			Assert.assertTrue(executeTest(iri, shexFile, rdfFile));
 		}catch (Exception e){
@@ -317,7 +317,7 @@ public class SKOSValidatorShexImplTest {
 	public void testAccept_19() {
 		try{
 			URI iri = new URI("http://example.org/Love");
-			String shexFile = "src/test/resources/skos/test/shex/rules/skos-concept.shex";
+			String shexFile = "src/main/resources/skos/shex/rules/skos-concept.shex";
 			String rdfFile = "src/test/resources/skos/test/sources/accepted/consistent-19.ttl";
 			Assert.assertTrue(executeTest(iri, shexFile, rdfFile));
 		}catch (Exception e){
@@ -332,7 +332,7 @@ public class SKOSValidatorShexImplTest {
 	public void testAccept_20() {
 		try{
 			URI iri = new URI("http://example.org/MyConcept");
-			String shexFile = "src/test/resources/skos/test/shex/rules/skos-concept.shex";
+			String shexFile = "src/main/resources/skos/shex/rules/skos-concept.shex";
 			String rdfFile = "src/test/resources/skos/test/sources/accepted/consistent-20.ttl";
 			Assert.assertTrue(executeTest(iri, shexFile, rdfFile));
 		}catch (Exception e){
@@ -345,7 +345,7 @@ public class SKOSValidatorShexImplTest {
 	public void testAccept_21() {
 		try{
 			URI iri = new URI("http://example.org/Potassium");
-			String shexFile = "src/test/resources/skos/test/shex/rules/skos-concept.shex";
+			String shexFile = "src/main/resources/skos/shex/rules/skos-concept.shex";
 			String rdfFile = "src/test/resources/skos/test/sources/accepted/consistent-21.ttl";
 			Assert.assertTrue(executeTest(iri, shexFile, rdfFile));
 		}catch (Exception e){
@@ -358,7 +358,7 @@ public class SKOSValidatorShexImplTest {
 	public void testAccept_22() {
 		try{
 			URI iri = new URI("http://example.org/MyResource");
-			String shexFile = "src/test/resources/skos/test/shex/rules/skos-concept.shex";
+			String shexFile = "src/main/resources/skos/shex/rules/skos-concept.shex";
 			String rdfFile = "src/test/resources/skos/test/sources/accepted/consistent-22.ttl";
 			Assert.assertTrue(executeTest(iri, shexFile, rdfFile));
 		}catch (Exception e){
@@ -372,7 +372,7 @@ public class SKOSValidatorShexImplTest {
 	public void testAccept_23() {
 		try{
 			URI iri = new URI("http://example.org/MyResource");
-			String shexFile = "src/test/resources/skos/test/shex/rules/skos-concept.shex";
+			String shexFile = "src/main/resources/skos/shex/rules/skos-concept.shex";
 			String rdfFile = "src/test/resources/skos/test/sources/accepted/consistent-23.ttl";
 			Assert.assertTrue(executeTest(iri, shexFile, rdfFile));
 		}catch (Exception e){
@@ -386,7 +386,7 @@ public class SKOSValidatorShexImplTest {
 	public void testAccept_24() {
 		try{
 			URI iri = new URI("http://example.org/Protein");
-			String shexFile = "src/test/resources/skos/test/shex/rules/skos-concept.shex";
+			String shexFile = "src/main/resources/skos/shex/rules/skos-concept.shex";
 			String rdfFile = "src/test/resources/skos/test/sources/accepted/consistent-24.ttl";
 			Assert.assertTrue(executeTest(iri, shexFile, rdfFile));
 		}catch (Exception e){
@@ -406,7 +406,7 @@ public class SKOSValidatorShexImplTest {
 	@Test
 	public void testRejected_2() throws URISyntaxException, IOException {
 		URI iri = new URI("http://example.org/MyConcept");
-		String shexFile = "src/test/resources/skos/test/shex/rules/skos-concept.shex";
+		String shexFile = "src/main/resources/skos/shex/rules/skos-concept.shex";
 		String rdfFile = "src/test/resources/skos/test/sources/rejected/rejected-2.ttl";
 		SKOSValidatorShexImpl validator = new SKOSValidatorShexImpl(shexFile, iri);
 		Assert.assertFalse(validator.validate(rdfFile));
