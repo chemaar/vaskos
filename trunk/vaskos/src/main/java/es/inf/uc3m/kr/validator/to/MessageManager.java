19b6f225131ec61ebaa53ab0a8d9076eff0f7893
chemaar <chema.ar@gmail.com>
2014-05-30 13:09:41 +0200

    Adding message manager

---
+++ trunk/vaskos/src/main/java/es/inf/uc3m/kr/validator/to/MessageManager.java
@@ -0,0 +1,11 @@
+package es.inf.uc3m.kr.validator.to;
+
+import java.util.List;
+
+public class MessageManager {
+
+	protected List<MessageTO> info;
+	protected List<MessageTO> warnings;
+	protected List<MessageTO> errors;
+	protected List<MessageTO> suggestions;
+}
---
+++ trunk/vaskos/src/main/java/es/inf/uc3m/kr/validator/to/MessageType.java
@@ -0,0 +1,5 @@
+package es.inf.uc3m.kr.validator.to;
+
+public enum MessageType {
+INFO, WARNING, ERROR, SUGGESTION
+}
