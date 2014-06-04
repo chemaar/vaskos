fac7eec5d3f14c9179d27d05a89376103097e8fd
chemaar <chema.ar@gmail.com>
2014-06-04 11:40:46 +0200

    Adding test

---
+++ trunk/vaskos-web/src/test/java/es/inf/uc3m/kr/vaskos/services/VaskosServiceRestTest.java
@@ -0,0 +1,39 @@
+package es.inf.uc3m.kr.vaskos.services;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+
+import junit.framework.Assert;
+import main.ValidatorAppServ;
+
+import org.junit.Test;
+
+import es.inf.uc3m.kr.vaskos.to.ValidationContext;
+import es.inf.uc3m.kr.vaskos.utils.SPARQLRulesLoader;
+
+public class VaskosServiceRestTest {
+
+	private static ValidationContext createValidationContext(String urlFile,
+			String[] sparqlFiles) {
+		ValidationContext vc = new ValidationContext();
+		vc.setUriFile(urlFile);
+		try {
+			vc.setSparqlFiles(sparqlFiles);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return vc;
+	}
+	
+	@Test
+	public void testSimpleValidateHTTPS() {
+		String urlFile = "https://raw.githubusercontent.com/chemaar/vaskos/master/trunk/vaskos/src/test/resources/skos/sparql/test/accepted/mountain-bike-simple.ttl";
+		String[] sparqlFiles = SPARQLRulesLoader.getSPARQLRuleFiles();
+		ValidationContext vc = createValidationContext(urlFile, sparqlFiles);
+		ValidatorAppServ validator = new ValidatorAppServ();
+		Assert.assertTrue(validator.simpleValidation(vc).isValid());
+	}
+
+
+}
